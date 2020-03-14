d90e2e9838f9a984effef64c901398c21290f10d
Jordan Morris <jthecybertinkerer@gmail.com>
2020-03-14 05:36:30 -0500

    Autocommit 2/6/2020 5:36:30.85 CST: Jordan Morris (jthecybertinkerer@gmail.com) updated file at 'server/index.js'

--- server/index.js
+++ server/index.js
@@ -8,7 +8,10 @@ const app = express();
 app.get("/api/products", (req, res) => {
   res.json(getProducts());
 });
-
+app.get("api/products/:id",(req,res)=>{
+    if()
+    res.json(getProduct(req.param.id))
+})
 app.listen(8080, () => {
   console.log("listening on " + "8080");
 });
