778c86a02a55c66b58bbbf78c11a8a118892f801
Jordan Morris <jthecybertinkerer@gmail.com>
2020-03-14 05:33:37 -0500

    Autocommit 2/6/2020 5:33:37.149 CST: Jordan Morris (jthecybertinkerer@gmail.com) updated file at 'server/getProducts.js'

--- server/getProducts.js
+++ server/getProducts.js
@@ -1,7 +1,5 @@
 const products = require("./products.json");
 
-module.exports = (req, res, products) => {
-  console.log(req, res, products);
-  console.log("this should return products:", this);
+module.exports = () => {
   return products;
 };
