2720a7659480dc45fb6d2c1de4c2e4e12808dd29
Jordan Morris <jhecybertinkerer@gmail.com>
2020-03-14 18:18:42 -0500

    Autocommit 2/6/2020 18:18:42.657 CST: Jordan Morris (jhecybertinkerer@gmail.com) updated file at 'server/getProduct.js'

--- server/getProduct.js
+++ server/getProduct.js
@@ -4,7 +4,7 @@ module.exports = requestedId => {
   console.log("requested id", requestedId);
   return products.find(product => {
     // console.log("getProduct id", product, "requestedID:");
-    console.log("product id", product.id);
+    console.log("typeof product id", typeof product.id);
     return product.id === requestedId;
   });
 };
