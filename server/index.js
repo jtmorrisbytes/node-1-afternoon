bdddf254ce0b4bccbd3d2e1531ed5b74b1bd0ef2
Jordan Morris <jthecybertinkerer@gmail.com>
2020-03-14 05:40:31 -0500

    Autocommit 2/6/2020 5:40:31.679 CST: Jordan Morris (jthecybertinkerer@gmail.com) updated file at 'server/index.js'

--- server/index.js
+++ server/index.js
@@ -12,7 +12,12 @@ app.get("api/products/:id", (req, res) => {
   if (!req.param.id) {
     res.status(409).send("missing field id");
   } else {
-    res.json(getProduct(req.param.id));
+    let product = getProduct(req.param.id);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).send("product not found?");
+    }
   }
 });
 app.listen(8080, () => {
