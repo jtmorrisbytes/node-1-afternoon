3705faae8ca9c7e1e6fafd3f9758e4e8cfdbf60e
Jordan Morris <jhecybertinkerer@gmail.com>
2020-03-14 19:28:15 -0500

    Autocommit 2/6/2020 19:28:15.27 CST: Jordan Morris (jhecybertinkerer@gmail.com) updated file at 'server/getProducts.js'

--- server/getProducts.js
+++ server/getProducts.js
@@ -4,6 +4,9 @@ module.exports = queryParamsObj => {
   if (queryParamsObj) {
     let result = products.slice();
     for (key in queryParamsObj) {
+      result = result.map(product => {
+        return product[key] == queryParamsObj[key];
+      });
     }
   } else {
     return products;
